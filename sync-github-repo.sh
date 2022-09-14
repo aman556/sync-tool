3510dfecbf22a722ede1f53041e7feeb329fd218
Aman Sharma <amansh@vmware.com>
2022-09-14 13:33:25 +0530

    correcting git frych command
    Signed-off-by: Aman Sharma <amansh@vmware.com>

--- sync-github-repo.sh
+++ sync-github-repo.sh
@@ -2,8 +2,8 @@
 
 syncingBranch=$1
 
-git fetch upstream upstream/"${syncingBranch}"
+git fetch upstream "${syncingBranch}"
 
 git checkout -b "${syncingBranch}"
 
-git rebase upstream/"${syncingBranch}"/"${syncingBranch}"
+git rebase upstream/"${syncingBranch}"
