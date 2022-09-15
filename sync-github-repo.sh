3864564e686b890dca0bfc07261c454845e4b7b7
Aman Sharma <amansh@vmware.com>
2022-09-15 16:12:33 +0530

    cloning the midstream in temp dir and pushing back after merging
    Signed-off-by: Aman Sharma <amansh@vmware.com>

--- sync-github-repo.sh
+++ sync-github-repo.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
 
-syncingBranch=$1
+upstreamRepo=$1
+upstreamBranch=$2
+midstreamRepo=$3
+midstreamBranch=$4
 
-git fetch upstream
+tmp_dir=$(mktemp -d -t syncrepo)
 
-git checkout -b "${syncingBranch}"
+cd "${tmp_dir}" || exit
 
-git rebase "${syncingBranch}" remotes/upstream/"${syncingBranch}"
+git clone "${midstreamRepo}"
+
+midstreamRepoName=$(ls)
+cd "${midstreamRepoName}" || exit
+
+git remote add upstream "${upstreamRepo}"
+
+git fetch upstream "${upstreamBranch}"
+
+# checkout to downstream branch and creating if not exist
+git checkout "${midstreamBranch}" || git checkout -b "${midstreamBranch}"
+
+# Only covered the case when upstream is ahead of midstream
+# ToDo: Cover the below case
+#       - When midstream and upstream both have some commits 
+#         then have resolve the merg conflict first
+if [ -n "$(git diff remotes/upstream/"${upstreamBranch}" "${midstreamBranch}")" ]
+then
+    git merge remotes/upstream/"${upstreamBranch}"
+    git push "${midstreamRepo}" "${midstreamBranch}"
+else
+    echo "Both branches are already Synced."
+fi
+
+rm -rf "${tmp_dir}"
